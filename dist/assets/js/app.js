f58378d84d1d683748ed0bacf417b6482fa9c56e
Sergey Nacharov <nacharovsa@it2g.ru>
2022-11-12 22:43:41 +0400

    fix

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -264,6 +264,38 @@ $(() => {
                 // yPercent: 0,
             })
     });
+
+    $(() => {
+        // var el = document.querySelector('payment');
+        // var rect = el.getBoundingClientRect();
+        // var mouse = {x: 0, y: 0, moved: false};
+        //
+        // $(".payment").mousemove(function(e) {
+        //     mouse.moved = true;
+        //     mouse.x = e.clientX - rect.left;
+        //     mouse.y = e.clientY - rect.top;
+        // });
+        //
+        // // Ticker event will be called on every frame
+        // TweenLite.ticker.addEventListener('tick', function(){
+        //     if (mouse.moved){
+        //         parallaxIt(".slide", -100);
+        //         parallaxIt("img", -30);
+        //     }
+        //     mouse.moved = false;
+        // });
+        //
+        // function parallaxIt(target, movement) {
+        //     TweenMax.to(target, 0.5, {
+        //         x: (mouse.x - rect.width / 2) / rect.width * movement,
+        //         y: (mouse.y - rect.height / 2) / rect.height * movement
+        //     });
+        // }
+        //
+        // $(window).on('resize scroll', function(){
+        //     rect = $('#container')[0].getBoundingClientRect();
+        // })
+    })
     // $(() => {
     //     // products
     //     const productsAnimation = gsap.timeline({
